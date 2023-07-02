58ab8293a818db5878124c0ebbc7e31305837835
AndyHou <andyhou965@gmail.com>
2023-07-02 18:37:05 +0400

    first commit

---
+++ apikey-template.py
@@ -0,0 +1,2 @@
+# Put your OpenAI API key here and rename this file to apikey.py
+OPENAI_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
---
+++ htmlTemplates.py
@@ -0,0 +1,29 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+	background-color: #898989;
+}
+.chat-message.bot {
+	background-color: #003fa5;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="message">{{MSG}}</div>
+</div>
+'''
